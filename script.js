e50c9ea7b5e1a2b1b2a5578b8bef9efc4113e949
4rlm <4rlm@protonmail.ch>
2022-08-17 04:31:35 +0900

    complete some code refactoring

--- script.js
+++ script.js
@@ -2,17 +2,15 @@ const videoElement = document.getElementById('video');
 const button = document.getElementById('button');
 
 // Prompt to select media stream, pass to video element, then play
-
 async function selectMediaStream() {
   try {
     const mediaStream = await navigator.mediaDevices.getDisplayMedia();
     videoElement.srcObject = mediaStream;
-    videoElement.onloadmetadata = () => {
+    videoElement.onloadedmetadata = () => {
       videoElement.play();
-    }
+    };
   } catch (error) {
-    // Catch Error
-    console.log('whoops, error here:', error);
+    // Catch Error Here
   }
 }
 
@@ -27,4 +25,3 @@ button.addEventListener('click', async () => {
 
 // On Load
 selectMediaStream();
-
